72c5153228554a6a781a7f5dcaf2ee04889ffcae
Maciek Sitkowski <msitkowski94@gmail.com>
2023-11-24 22:24:54 +0100

    fix unresolved internal import pattern
    https://github.com/import-js/eslint-import-resolver-typescript?tab=readme-ov-file#configuration

--- apps/remix/.eslintrc.cjs
+++ apps/remix/.eslintrc.cjs
@@ -1,4 +1,15 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['@company/eslint-config/base', '@company/eslint-config/react'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        project: 'apps/remix/tsconfig.json',
+      },
+    },
+    'import/internal-regex': '^~/',
+  },
 }
