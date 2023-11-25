d8ccb1d4d9695dfc843556f7da76dec4b2f19cac
Maciek Sitkowski <msitkowski94@gmail.com>
2023-11-25 16:33:23 +0100

    run validation jobs in parallel

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - [x] manually deploy to fly.io
 - [x] create CI deployment workflow to fly.io
 - [x] improve CI deployment workflow to trigger only for changed apps
-- [ ] run typecheck, lint, test and build in parallel
+- [x] run typecheck, lint, test and build in parallel
 - [ ] setup tailwindcss in remix
 - [ ] create shared ui lib
 - [ ] setup Storybook in shared ui lib
@@ -20,6 +20,7 @@
 - [x] set unified path aliases for all apps and shared libs (done for `apps/`, because `libs/` probably don't need them anyway)
 - [x] add unused imports plugin to eslint
 - [ ] research if it's worth using `turbo`
+- [ ] create diagram
 
 ## References
 
--- apps/nestjs/src/app.controller.spec.ts
+++ apps/nestjs/src/app.controller.spec.ts
@@ -2,14 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { DogsController } from './dogs/dogs.controller'
+import { DogsService } from './dogs/dogs.service'
 
 describe('AppController', () => {
   let appController: AppController
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [AppController, DogsController],
+      providers: [AppService, DogsService],
     }).compile()
 
     appController = app.get<AppController>(AppController)
