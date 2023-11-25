26da30fcd2b4fb0b57f064d1eef62d13cd5765cb
Maciek Sitkowski <msitkowski94@gmail.com>
2023-11-25 12:12:01 +0100

    add deployment job for nestjs

--- apps/nestjs/src/app.controller.spec.ts
+++ apps/nestjs/src/app.controller.spec.ts
@@ -16,8 +16,8 @@ describe('AppController', () => {
   })
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!')
+    it('contains "Hello World"', () => {
+      expect(appController.getHello()).toContain('Hello World')
     })
   })
 })
