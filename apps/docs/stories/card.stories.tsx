97d8b16813a277d2bf64cc6eeedbf5f44cef882e
Maciek Sitkowski <msitkowski94@gmail.com>
2023-11-26 04:47:02 +0100

    add card story

---
+++ apps/docs/stories/card.stories.tsx
@@ -0,0 +1,20 @@
+import { Card } from '@repo/ui'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Card> = {
+  component: Card,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Card>
+
+export const Primary: Story = {
+  render: props => <Card {...props}>Hello</Card>,
+  name: 'Card',
+  args: {
+    children: 'Hello',
+    title: 'Card Title',
+    href: '#some-link',
+  },
+}
